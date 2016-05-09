f1e9c340ff0529cc3470f91af17ec3d385c6786c
linyaosky <1245807623@qq.com>
2016-05-09 21:58:03 +0800

    整理代码

---
+++ src/main/java/me/skyline/modules/login/LoginController.java
@@ -0,0 +1,26 @@
+package me.skyline.modules.login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 处理登录请求
+ * @author chendh
+ * 2016年5月9日
+ */
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login")
+	@ResponseBody
+	public String login(HttpServletRequest request, HttpServletResponse response, Model model){
+		String message = "springMVC ok";
+		model.addAttribute("name", "cdh");
+		return message ;
+	}
+}
---
+++ src/test/java/skyline/sort/BubbleSort.java
@@ -0,0 +1,7 @@
+package skyline.sort;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class BubbleSort {
+}
