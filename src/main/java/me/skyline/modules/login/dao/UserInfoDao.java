00ef773d0c44cb09ee0d9a5548c742af035a2a66
linyaosky <1245807623@qq.com>
2016-05-10 22:33:54 +0800

    配置MyBatis

---
+++ src/main/java/me/skyline/modules/login/dao/UserInfoDao.java
@@ -0,0 +1,12 @@
+package me.skyline.modules.login.dao;
+
+import me.skyline.modules.login.entity.UserInfoEntity;
+
+/**
+ * 用户信息dao
+ * @author chendh
+ * 2016年5月10日
+ */
+public interface UserInfoDao {
+	public void insertUserInfo(UserInfoEntity user);
+}
