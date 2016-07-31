088fc9cc08d2e42a274f68105abd2250dc7d26af
linyaosky <1245807623@qq.com>
2016-07-31 22:47:22 +0800

    完善实体类

--- src/main/java/me/skyline/modules/login/entity/UserInfoEntity.java
+++ src/main/java/me/skyline/modules/login/entity/UserInfoEntity.java
@@ -19,4 +19,101 @@ public class UserInfoEntity {
 	private int type;
 	private String clientType;
 	private String clientVersion;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLoginName() {
+		return loginName;
+	}
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getLoginIp() {
+		return loginIp;
+	}
+	public void setLoginIp(String loginIp) {
+		this.loginIp = loginIp;
+	}
+	public Date getLastLoginTime() {
+		return lastLoginTime;
+	}
+	public void setLastLoginTime(Date lastLoginTime) {
+		this.lastLoginTime = lastLoginTime;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getLastUpTime() {
+		return lastUpTime;
+	}
+	public void setLastUpTime(Date lastUpTime) {
+		this.lastUpTime = lastUpTime;
+	}
+	public int getDelFlg() {
+		return delFlg;
+	}
+	public void setDelFlg(int delFlg) {
+		this.delFlg = delFlg;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getClientType() {
+		return clientType;
+	}
+	public void setClientType(String clientType) {
+		this.clientType = clientType;
+	}
+	public String getClientVersion() {
+		return clientVersion;
+	}
+	public void setClientVersion(String clientVersion) {
+		this.clientVersion = clientVersion;
+	}
+	
 }
